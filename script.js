12cef9bfc85640b02a11b08ef4def72cff522637
La-Ronda <lalacha@macbookpro.mynetworksettings.com>
2025-02-15 12:19:24 -0600

    My second commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const fruits = [
+    "apples",
+    "bananas",
+    "cherries",
+    "dates",
+    "elderberries",
+    "figs",
+    "grapes",
+  ];
+  
+  // 1
+  // How many elements are in the fruits array?
+  
+  // 2
+  // Retrieve the first and last elements of fruits array
+  
+  const colors = ["red", "green", "blue", "black", "white"];
+  
+  // 3
+  // Retriever the first and the second elements from the colors array
+  // Hint: Usee array destructuring
+  
+  // 4
+  // Change the first element of colors array to orange
+  
+  // 5
+  // Change the third element of colors array to teal
+  
+  // 6
+  // Combine fruits array with colors array and assign it to a new array called colorfulFruits
+  // Hint: Use spread operator and then use concat operator
+  
+  // 7
+  // Remove the first element from the fruits array
+  
+  // 8
+  // Remove the last element from the fruits array
+  
+  // 9
+  // Add your favorite fruit to the beginning of the fruits array
+  
+  // 10
+  // Add your second favorite fruit to the end of the fruits array
+  
+  const numbers = [1, 2, 3, 2, 4, 2, 5];
+  
+  // 11
+  // Find the index of the first occurrence of 2 in the numbers array
+  
+  // 12
+  // Find the index of the last occurrence of 2 in the numbers array
+  
+  // 13
+  // Check if numbers array contains 3
+  
+  // 14
+  // Check if numbers array contains 9
