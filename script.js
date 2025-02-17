53eb38a896a44ba4e03d3e3ce383a835b310093e
La-Ronda <lalacha@macbookpro.mynetworksettings.com>
2025-02-16 18:36:28 -0600

    completed 1 -4 on assignment

--- script.js
+++ script.js
@@ -10,18 +10,35 @@ const fruits = [
   
   // 1
   // How many elements are in the fruits array?
+
+  console.log(fruits.length);
+
+  const totalFruits(fruits.length);
   
   // 2
   // Retrieve the first and last elements of fruits array
-  
-  const colors = ["red", "green", "blue", "black", "white"];
+  const firstElement = fruits.find((element1) => element1.length > 1  );
+  const LastElement = fruits.findLast((element2) => element2.length < 7);
+ console.log ( "You have "+ totalFruits +"fruits on your list. The First Fruit on your list is "+ firstElement + " and the last fruit on your list is "+ LastElement +);
   
   // 3
   // Retriever the first and the second elements from the colors array
   // Hint: Usee array destructuring
+  const colors = ["red", "green", "blue", "black", "white"];
+  // first element color destructuring
+  const firstElementcolor = colors.find((colorElement1) => colorElement1.length > 1  );
+  // Second element color destructuring
+  const LastElementcolor = colors.findLast((colorElement2) => colorElement2.length < 7);
+
+  // prints out the firs and second elements of the colors array
+ console.log ("The first color on your list is "+ firstElement + " and the last color on your list is "+ LastElement +);
   
+
   // 4
   // Change the first element of colors array to orange
+
+const replaceFirstelement = colors.replace("red","orange");
+
   
   // 5
   // Change the third element of colors array to teal
